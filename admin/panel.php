f9b7c76a4c720494634f6be5e0b1828d154c2a7e
k0r37k1 <64130853+k0r37k1@users.noreply.github.com>
2025-04-17 21:30:49 +0200

    Update panel.php

--- admin/panel.php
+++ admin/panel.php
@@ -29,6 +29,15 @@ $config = sf_getConfig();
       <div class="help">Show image thumbnails in post lists</div>
     </div>
     
+    <div class="form-group">
+      <label for="default_markdown">Default to Markdown:</label>
+      <select id="default_markdown" name="default_markdown">
+        <option value="1" <?php echo ($config['default_markdown'] ?? true) ? 'selected' : ''; ?>>Yes</option>
+        <option value="0" <?php echo isset($config['default_markdown']) && !$config['default_markdown'] ? 'selected' : ''; ?>>No</option>
+      </select>
+      <div class="help">Use Markdown as default format for new posts</div>
+    </div>
+    
     <div class="form-actions">
       <button type="submit" class="btn-primary">Save Settings</button>
     </div>
@@ -38,5 +47,18 @@ $config = sf_getConfig();
     <h3>Management</h3>
     <a href="?page=simplefeed&action=list" class="btn-secondary">View all posts</a>
     <a href="?page=simplefeed&action=edit" class="btn-secondary">Create new post</a>
+  </div>
+  
+  <div class="markdown-info">
+    <h3>Markdown Support</h3>
+    <p>SimpleFeed now supports Markdown formatting for your posts. You can enable or disable Markdown individually for each post when editing.</p>
+    <p>Benefits of using Markdown:</p>
+    <ul>
+      <li>Simpler and cleaner syntax compared to HTML</li>
+      <li>Easier to read and write</li>
+      <li>Prevents common HTML errors</li>
+      <li>Better security - automatically sanitized</li>
+    </ul>
+    <p>The editor provides a helpful Markdown cheat sheet when you're editing a post.</p>
   </div>
 </div>
