89347ba353b594ef6f912fe33b1b2536da941921
k0r37k1 <64130853+k0r37k1@users.noreply.github.com>
2025-04-17 10:43:33 +0200

    Delete actions directory

--- actions/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($_GET['slug'])) die('Kein Beitrag angegeben.');
-$slug = basename($_GET['slug']);
-$path = __DIR__ . '/../data/' . $slug . '.json';
-if (file_exists($path)) unlink($path);
-header('Location: ../?page=simplefeed');
